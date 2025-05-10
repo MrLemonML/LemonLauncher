ec7a95bc7d2b8ab0366ae6395924ec877b6b65f7
MrLemonML <nevi.bt1+1@gmail.com>
2025-05-10 07:31:00 +0200

    s

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -4,7 +4,7 @@ const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
 // exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
-exports.REMOTE_DISTRO_URL = 'https://helios-files.geekcorner.eu.org/distribution.json'
+exports.REMOTE_DISTRO_URL = 'https://raw.githubusercontent.com/MrLemonML/nebula/a822c2b3380ef343f16c53c0a52fb40286bc43d8/distribution.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
